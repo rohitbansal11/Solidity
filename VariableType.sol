f7e5961b7e34380e5e8c70ba250329f0539e787e
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2023-06-27 11:52:21 +0530

    Create VariableType.sol

---
+++ VariableType.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.11;
+
+contract MyContract{
+    bool ischeck ;
+    uint a;
+    address reciption;
+    bytes32 data;
+
+
+    string name ;
+    bytes _data;
+    uint[] amount;
+    mapping(uint =>string)users;
+
+
+    struct user {
+        uint a;
+        string name ;
+        uint[] frindgs;
+
+    }
+    enum Color {
+        RED,
+        GREEN,
+        BLUE
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
