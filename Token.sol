be7d9d1d37a06667a9615092b3ede18fafeed6c3
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2023-07-13 12:38:38 +0530

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+
+
+
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract Token is ERC20{}
